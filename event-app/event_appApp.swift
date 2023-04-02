3e70d65494c4dddac3860ffea5213aa8ba33764b
Benedict Kunzmann <bku@sevenstax-intern.de>
2023-04-02 18:59:09 +0200

    Initial Commit

---
+++ event-app/event_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_appApp.swift
+//  event-app
+//
+//  Created by Benedict Kunzmann on 02.04.23.
+//
+
+import SwiftUI
+
+@main
+struct event_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
