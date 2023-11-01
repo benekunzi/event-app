b7f97ec1a82fbd797d89d98e4fdf021298d23268
Benedict Kunzmann <benekunzi@gmail.com>
2023-11-01 21:14:12 +0100

    Switched from GoogleMaps to AppleMaps

---
+++ event-app/Tab-House/AppleMaps/EventAppleMapPreviewView.swift
@@ -0,0 +1,8 @@
+//
+//  EventAppleMapPreviewView.swift
+//  event-app
+//
+//  Created by Benedict Kunzmann on 01.11.23.
+//
+
+import Foundation
---
+++ event-app/Tab-House/AppleMaps/EventAppleMapView.swift
@@ -0,0 +1,8 @@
+//
+//  EventAppleMapView.swift
+//  event-app
+//
+//  Created by Benedict Kunzmann on 01.11.23.
+//
+
+import Foundation
